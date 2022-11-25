539384e6e7c1c5b008afb84ed5659cfc3d3da383
Huseyin Kan <huseyin.kann@hotmail.com>
2022-11-25 13:21:49 +0300

    web api started

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,3 +1,7 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +29,8 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IProductService,ProductManager>();
+            services.AddSingleton<IProductDal, EfProductDal>();
             services.AddControllers();
         }
 
